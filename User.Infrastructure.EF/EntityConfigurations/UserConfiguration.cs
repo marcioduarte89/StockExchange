63bb794352f09f1ea1c7d9a2db519f2bf435fb4c
Marcio Duarte <mduarte@freewheel.com>
2024-06-04 20:32:06 +0100

    initial commit

---
+++ User.Infrastructure.EF/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace User.Infrastructure.EF.EntityConfigurations;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<Domain.User>
+{
+    public void Configure(EntityTypeBuilder<Domain.User> builder)
+    {
+        builder
+            .Property(x => x.Id)
+            .UseIdentityAlwaysColumn();
+
+        builder.Property(a => a.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.UserName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(m => m.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(m => m.PhoneNumber)
+            .HasMaxLength(30);
+    }
+}
