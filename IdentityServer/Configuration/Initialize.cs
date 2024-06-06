3bb9634a339ab18ba231c22e3fb9f00f27ed90f1
Marcio Duarte <mduarte@freewheel.com>
2024-06-06 20:54:53 +0100

    identity

---
+++ IdentityServer/Configuration/Initialize.cs
@@ -0,0 +1,43 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Configuration;
+
+public class Initialize
+{
+    public static void InitializeDatabase(WebApplication app)
+    {
+        using var serviceScope = app.Services.GetService<IServiceScopeFactory>()!.CreateScope();
+        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        context.Database.Migrate();
+        if (!context.Clients.Any())
+        {
+            foreach (var client in Config.Clients)
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            foreach (var resource in Config.IdentityResources)
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+            foreach (var resource in Config.ApiScopes)
+            {
+                context.ApiScopes.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+    }
+}
